91d8da0ee0b6d0fb89575ae41d107b54e8a30cb0
Carlos Melegrito <cj@mlgrto.com>
2020-02-10 19:49:36 +1100

    Initial Commit

---
+++ NAECompact/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NAECompact
+//
+//  Created by Carlos Melegrito on 10/2/20.
+//  Copyright © 2020 Carlos Melegrito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
