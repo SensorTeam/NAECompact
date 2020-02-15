232abc0227b6de730a0901424a3369794163edd0
Carlos Melegrito <cj@mlgrto.com>
2020-02-15 22:00:54 +1100

    Start new version

---
+++ NAECompact/Sources/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  NAECompact
+//
+//  Created by Carlos Melegrito on 15/2/20.
+//  Copyright © 2020 Carlos Melegrito. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
